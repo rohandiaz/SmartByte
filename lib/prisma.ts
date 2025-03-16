1612f1cd83886d77a3772df4ccde4df52abe6081
rohandiaz <rohanamylasedas@gmail.com>
2025-03-16 19:03:18 +0530

    Refactored Prisma Client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,21 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-// This approach prevents multiple instances of Prisma Client in development
-// https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+// Prevent multiple instances of Prisma Client in development
+const globalForPrisma = globalThis as unknown as { cachedPrisma?: PrismaClient };
 
-declare global {
-  var cachedPrisma: PrismaClient;
-}
+const db = globalForPrisma.cachedPrisma ?? new PrismaClient();
 
-let db: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  db = new PrismaClient();
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
-  }
-  db = global.cachedPrisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.cachedPrisma = db;
 }
 
 export { db };
